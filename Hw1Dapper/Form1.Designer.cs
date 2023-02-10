a7cd8f4e9831cb3159b35cad2e6dcc701b43d958
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2023-02-11 02:16:22 +0400

    Add project files.

---
+++ Hw1Dapper/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Hw1Dapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClaer = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tctboxSearch = new System.Windows.Forms.TextBox();
+            this.listBoxProduct = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnClaer
+            // 
+            this.btnClaer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClaer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.btnClaer.Location = new System.Drawing.Point(12, 394);
+            this.btnClaer.Name = "btnClaer";
+            this.btnClaer.Size = new System.Drawing.Size(776, 29);
+            this.btnClaer.TabIndex = 5;
+            this.btnClaer.Text = "Claer List";
+            this.btnClaer.UseVisualStyleBackColor = false;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.btnRemove.Location = new System.Drawing.Point(12, 359);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(776, 29);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove Product";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.btnDelete.Location = new System.Drawing.Point(12, 324);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(776, 29);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Edit Product";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.btnAdd.Location = new System.Drawing.Point(12, 289);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(776, 29);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add Product";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // tctboxSearch
+            // 
+            this.tctboxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tctboxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.tctboxSearch.ForeColor = System.Drawing.Color.Black;
+            this.tctboxSearch.Location = new System.Drawing.Point(12, 28);
+            this.tctboxSearch.Name = "tctboxSearch";
+            this.tctboxSearch.PlaceholderText = "  Search";
+            this.tctboxSearch.Size = new System.Drawing.Size(776, 27);
+            this.tctboxSearch.TabIndex = 4;
+            // 
+            // listBoxProduct
+            // 
+            this.listBoxProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(243)))), ((int)(((byte)(174)))));
+            this.listBoxProduct.FormattingEnabled = true;
+            this.listBoxProduct.ItemHeight = 20;
+            this.listBoxProduct.Location = new System.Drawing.Point(12, 59);
+            this.listBoxProduct.Name = "listBoxProduct";
+            this.listBoxProduct.Size = new System.Drawing.Size(776, 224);
+            this.listBoxProduct.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClaer);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tctboxSearch);
+            this.Controls.Add(this.listBoxProduct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnClaer;
+        private Button btnRemove;
+        private Button btnDelete;
+        private Button btnAdd;
+        private TextBox tctboxSearch;
+        private ListBox listBoxProduct;
+    }
+}
